1cba71ce9f969d976a1564a3ff28d446ff9247a2
fabrykoppany <koppany.fabry@gmail.com>
2021-04-22 00:28:43 +0300

    Implemented basic time conversion functions and procedures

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+#include "time_buses.h"
+
 int main() {
-    printf("Hello, World!\n");
+    TIME time_tester = createTime(12, 30);
+
+    /*time_tester.hour = 12;
+    time_tester.minute = 30;
+
+    time_tester.full_time = convertHMToFT(time_tester.hour, time_tester.minute);
+    convertFTToHM(time_tester.full_time, &time_tester.hour, &time_tester.minute);
+    strcpy(time_tester.time_str, convertHMToStr(time_tester.hour, time_tester.minute));
+    convertStrToHM(time_tester.time_str, &time_tester.hour, &time_tester.minute);*/
+
     return 0;
 }
--- time_buses.h
+++ time_buses.h
@@ -5,19 +5,31 @@
 #ifndef PROJECT_02_TIME_BUSES_H
 #define PROJECT_02_TIME_BUSES_H
 
-#include "buses.h"
-
-#include <stdio.h>
-#include <stdlib.h>
-
-/**
+/*
  * TIME data structure
- * full_time - "hour" converted into minutes + "minutes"
+ * full_time - "hour" converted into minutes + "minute"
  * time_str - time shown in digital form (HH:MM)
  */
 typedef struct{
     int hour, minute, full_time;
-    char time_str[5];
+    char time_str[6];
 }TIME;
+
+#include "buses.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+TIME createTime(int hour, int minute);
+int convertHMToFT(int hour, int minute);
+void convertFTToHM(int full_time, int *hour, int *minute);
+char* convertHMToStr(int hour, int minute);
+void convertStrToHM(char *time_str, int *hour, int *minute);
+bool isHour(int hour);
+bool isMinute(int minute);
+bool isTime(int hour, int minute);
+bool possibleTime(const char *time);
 
 #endif //PROJECT_02_TIME_BUSES_H
