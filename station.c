4a0d88d0cdf581ff5327543814347ecb1971c0a9
Daniel <daniel@tohka.us>
2021-04-22 22:12:40 +0300

    Bus stations can now be created

--- station.c
+++ station.c
@@ -3,3 +3,42 @@
 //
 
 #include "station.h"
+
+STATION createStation(const char *city_name, const char *station_name, int platfrom_capacity, int depot_capacity){
+    STATION station;
+
+    station.platform_capacity = platfrom_capacity;
+    station.depot_capacity = depot_capacity;
+    station.name = allocateMemory(20);
+    strcpy(station.name, station_name);
+    station.city = allocateMemory(20);
+    strcpy(station.city, city_name);
+    station.abbreviation = allocateMemory(5);
+    strcpy(station.abbreviation, createCityStationAbbreviation(station));
+
+    return station;
+}
+
+char *createCityStationAbbreviation(STATION station){
+    char *abbr = allocateMemory(5);
+
+    abbr[0] = station.city[0];
+    abbr[1] = station.city[1];
+    abbr[2] = station.name[0];
+    abbr[3] = station.name[1];
+    abbr[4] = '\0';
+
+    for (int i = 0; i < strlen(abbr); ++i){
+        abbr[i] = (char) toupper(abbr[i]);
+    }
+
+    return abbr;
+}
+
+char *getCityName(){
+    return getBuffer();
+}
+
+char *getStationName(){
+    return getBuffer();
+}
--- station.h
+++ station.h
@@ -8,9 +8,12 @@
 #include "buses.h"
 #include "bst_buses.h"
 #include "queue_buses.h"
+#include "buffer.h"
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 /*
  * STATION data structure
@@ -18,11 +21,18 @@
  * first - dynamic queue
  * city - name of the city where the station is
  * name - name of the station itself
+ * also stores both the capacity of its platform and depot
  */
 typedef struct{
     BST_Node *root;
     queueNode *first;
-    char city[20], name[20];
+    char *city, *name, *abbreviation;
+    int platform_capacity, depot_capacity;
 }STATION;
+
+STATION createStation(const char *city_name, const char *station_name, int platfrom_capacity, int depot_capacity);
+char *createCityStationAbbreviation(STATION station);
+char *getCityName();
+char *getStationName();
 
 #endif //PROJECT_02_STATION_H
